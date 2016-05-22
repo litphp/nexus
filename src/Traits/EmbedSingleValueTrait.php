c8913bb93ace3925499438e098f4bc96850e369b
McFog Wang <mcfogw@gmail.com>
2016-05-22 20:53:59 +0800

    add new key-value class & traits

---
+++ src/Traits/EmbedSingleValueTrait.php
@@ -0,0 +1,39 @@
+<?php namespace Lit\Nexus\Traits;
+
+use Lit\Nexus\Interfaces\ISingleValue;
+
+class EmbedSingleValueTrait
+{
+    /**
+     * @var ISingleValue
+     */
+    protected $innerSingleValue;
+
+    public function get()
+    {
+        return $this->innerSingleValue->get();
+    }
+
+    public function delete()
+    {
+        $this->innerSingleValue->delete();
+    }
+
+    public function exists()
+    {
+        return $this->innerSingleValue->exists();
+    }
+
+    public function set($value)
+    {
+        $this->innerSingleValue->set($value);
+    }
+
+    /**
+     * @return ISingleValue
+     */
+    public function getInnerSingleValue()
+    {
+        return $this->innerSingleValue;
+    }
+}
